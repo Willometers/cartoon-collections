9904b5d2a7a2c99ee301a309373675fe5e8ee2a6
BillyMiles <williammiles201@gmail.com>
2020-12-15 15:35:33 -0500

    commit tree 9c5ec590ea9c7092b76751be70d978966ea1811b

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves(names)
+  names.each_with_index {|name, idx| 
+    puts "#{idx+1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(calls)
+  calls.collect do |call| 
+    "#{call.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(assorted_words)
+    if assorted_words.any? {|word| word.length > 4}
+      true
+    else
+      false
+    end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find {|name| cheese_types.include?(name)}
 end
+# cheese_types = ["cheddar", "gouda", "camembert"]
